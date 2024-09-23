a327022dead53dd64af530eb93296897341dfc6a
Suzdalev Andrey <suzdalev2005@gmail.com>
2024-09-23 13:41:53 +0300

    Create UserService and PostService

---
+++ UserService/src/test/java/ru/andreyszdlv/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.andreyszdlv.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
